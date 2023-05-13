eeb65dc0ea98056122f2bbc2c85687564c7cb9ec
Léo Batouxas <leo.batouxas@gmail.com>
2023-05-13 11:58:33 +0200

    Add Entity Candidacy, Offer, Statut

---
+++ JobPortalIntranetLibraryClass/entities/Statut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortalIntranetLibraryClass.entities
+{
+    internal class Statut
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("STA_ID")]
+        public int id;
+
+        [Key]
+        [Column("STA_LIBELLE")]
+        public string libelle;
+
+        public ICollection<Offer> offers { get; set; }
+    }
+}
