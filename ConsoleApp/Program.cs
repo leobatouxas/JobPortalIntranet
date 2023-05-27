640d98e0ed284070cc0ac6f719a3dd7d484fd12e
leobatouxas <leo.batouxas@gmail.com>
2023-05-27 11:52:16 +0200

    add BusinessLayer and folder Console, Serveur, TestAdd stub in ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,13 +13,64 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             ContextFluent contexte = new ContextFluent();
+
+            // lecture des clients
+            List<Employe> employes = contexte.Employes.ToList();
+            Console.WriteLine("Liste de mes clients avec DA : ");
+            foreach (Employe c in employes)
+            {
+                Console.WriteLine("Client n°{0} : {1} {2}", c.Id, c.Firstname, c.Lastname);
+            }
+
+            Console.WriteLine(employes);
             Statut statut = new Statut();
             statut.Id = 1;
             statut.Libelle = "Libelle 1";
             contexte.Statuts.Add(statut);
+            
+
+            Offer offer = new Offer();
+            offer.Title = "Offer 1";
+            offer.Description = "descrition offer 1";
+            offer.Statut = statut;
+            offer.Responsible = "Responsible 1";
+            offer.Salary = 4500;
+            offer.Date = DateTime.Now;
+            contexte.Offers.Add(offer);
+
+            Employe employe = new Employe();
+            employe.Seniority = 1;
+            employe.Lastname = "lastname 1";
+            employe.Firstname = "firstname 1";
+            employe.Dateofbirth = DateTime.Now;
+            employe.Biography = "biographie 1";
+            contexte.Employes.Add(employe);
+
+            Experience experience = new Experience();
+            experience.Title = "experience 1";
+            experience.Date = DateTime.Now.AddDays(1);
+            experience.Employe = employe;
+            contexte.Experiences.Add(experience);
+
+            Training training = new Training();
+            training.Title = "title 1";
+            training.Date = DateTime.Now.AddDays(1);
+            training.Employe = employe;
+            contexte.Trainings.Add(training);
+
+            Candidacy candidacy = new Candidacy();
+            candidacy.Offer = offer;
+            candidacy.Employe = employe;
+            candidacy.Date = DateTime.Now.AddDays(1);
+            candidacy.Status = "Statut";
+            contexte.Candidacies.Add(candidacy);
+
             contexte.SaveChanges();
             Console.WriteLine("FINISH");
             Console.ReadLine();
+
+            
+            
         }
     }
 }
--- JobPortalIntranetLibraryClass/Properties/AssemblyInfo.cs
+++ JobPortalIntranetLibraryClass/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("JobPortalIntranetLibraryClass")]
+[assembly: AssemblyTitle("Modele")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JobPortalIntranetLibraryClass")]
+[assembly: AssemblyProduct("Modele")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
